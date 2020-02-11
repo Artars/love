f8910111da5108a442aae6de08def0208d3c8c69
Arthur Souza <arsbr.souza@gmail.com>
2020-02-10 23:10:21 -0300

    Change materials and add graphical settings

---
+++ Assets/Scripts/HUD/QualitySelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QualitySelector : MonoBehaviour
+{
+    public Dropdown dropdown;
+    public bool applyExpensiveChanges = false;
+    protected string[] qualityNames;
+    protected int currentSetting;
+    protected List<Dropdown.OptionData> dropdownOptions;
+    
+
+
+    void Start()
+    {
+        qualityNames = QualitySettings.names;
+        currentSetting = QualitySettings.GetQualityLevel();
+        
+        GenerateOptions();
+        dropdown.AddOptions(dropdownOptions);
+        dropdown.value = currentSetting;
+        dropdown.onValueChanged.AddListener(OnValueChanged);
+
+    }
+
+    void GenerateOptions()
+    {
+        dropdownOptions = new List<Dropdown.OptionData>();
+        foreach (var option in qualityNames)
+        {
+            dropdownOptions.Add(new Dropdown.OptionData(option));
+        }
+    }
+
+    public void OnValueChanged(int newIndex)
+    {
+        QualitySettings.SetQualityLevel(newIndex, applyExpensiveChanges);
+    }
+
+
+}
