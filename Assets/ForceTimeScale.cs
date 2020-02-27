07cf603e9b67fbd8c9e7fe3db5a38edbab030867
Arthur Souza <arsbr.souza@gmail.com>
2020-02-27 18:34:21 -0300

    Finished first draft

---
+++ Assets/ForceTimeScale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceTimeScale : MonoBehaviour
+{
+    [Range(0,2)]
+    public float timeScale = 1;
+
+    void Start()
+    {
+        Time.timeScale = timeScale;
+    }
+
+}
